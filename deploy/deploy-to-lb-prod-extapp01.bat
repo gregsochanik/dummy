0053ba5b9287a78f7e9899977fb5910129f17735
greg Sochanik <greg@sochanik.com>
2013-07-16 11:08:20 +0100

    Changed name of environment

--- deploy/deploy-to-lb-prod-extapp01.bat
+++
@@ -1,4 +0,0 @@
-REM - this is wokr in progress, ideally this will run like the other environs in an internal build
-
-REM - Pull all artifacts from TeamCity
-%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -file last-pinned-from-tc.ps1
